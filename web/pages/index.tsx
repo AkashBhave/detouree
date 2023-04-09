5d9d565195c166c9448656dd2c4d47d7e8e29ec6
Akash Bhave <akashbbhave@gmail.com>
2023-04-08 23:03:49 -0400

    Create map page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -11,6 +11,10 @@ import {
 } from "@chakra-ui/react";
 import Head from "next/head";
 import axios from "axios";
+import Map from "react-map-gl";
+import maplibregl from "maplibre-gl";
+
+import "maplibre-gl/dist/maplibre-gl.css";
 
 const Home = () => {
   return (
@@ -19,11 +23,40 @@ const Home = () => {
         <title>Detouree</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main style={{ height: "100%" }}>
-        <Center h="full" flexDir="column">
-          Welcome to Detouree!
-        </Center>
-      </main>
+      <Map
+        mapLib={maplibregl}
+        initialViewState={{
+          longitude: -76.9431575,
+          latitude: 38.9900915,
+          zoom: 14,
+        }}
+        style={{ width: "100%", height: "100%" }}
+        mapStyle={{
+          version: 8,
+          sources: {
+            basemap: {
+              type: "raster",
+              tiles: [
+                "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                "https://b.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              ],
+              tileSize: 256,
+              attribution:
+                'Map tiles by <a target="_top" rel="noopener" href="http://stamen.com">Stamen Design</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>',
+            },
+          },
+          layers: [
+            {
+              id: "basemap",
+              type: "raster",
+              source: "basemap",
+              minzoom: 0,
+              maxzoom: 22,
+            },
+          ],
+        }}
+      />
     </>
   );
 };
